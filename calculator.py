e27e6bbe13a86b38668e3f46c4770d52aba5e35c
Nitish Patel <nitishp74@gmail.com>
2025-06-08 18:12:27 +0530

    improved code readibility

--- calculator.py
+++ calculator.py
@@ -11,13 +11,17 @@ def add(numbers: str) -> int:
         delimiter, numbers = numbers[2:].split('\n', 1)
     else:
         delimiter = ','
+
     # Split the input string by commas and newlines
     nums_list = numbers.replace('\n', ',').split(delimiter)
+
     # checking for invalid characters
     invalid_chars = [num for num in nums_list if not num.isdigit() and not (
         num.startswith('-') and num[1:].isdigit())]
+
     if invalid_chars:
         raise ValueError(f"Invalid number: {', '.join(invalid_chars)}")
+
     # checking for negative numbers
     negatives = [num for num in nums_list if int(num) < 0]
     if negatives:
